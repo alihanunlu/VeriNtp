47e68583fc810959773bde8adefc23faee6cf908
Alihan ÜNLÜ <alihan3134@gmail.com>
2021-04-25 00:31:26 +0300

    denme

--- VeriNtp/Form1.cs
+++ VeriNtp/Form1.cs
@@ -16,5 +16,10 @@ namespace VeriNtp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("sad");
+        }
     }
 }
